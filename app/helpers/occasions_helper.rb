e058f7c60e1ef762845adb0b04819e7dc66fbe4b
Dalal Alrayes <dalrayes@gmail.com>
2016-01-26 23:34:59 -0600

    Adds helpers for types of occasions and types of recipients for forms

---
+++ app/helpers/occasions_helper.rb
@@ -0,0 +1,14 @@
+module OccasionsHelper
+	def occasion
+		[
+			['Birthday', 'birthday'],
+			['Anniversary', 'anniversary'],
+			["Valentine's day", 'valentines_day'],
+			["Mothers's day", 'mothers_day'],
+			["Father's day", 'fathers_day'],			
+			["Grandparents' day", 'grandparents_day'],
+			["Holidays", 'holidays'],
+			["Corporate", 'Corporate']
+		]
+	end
+end
---
+++ app/helpers/recipients_helper.rb
@@ -0,0 +1,28 @@
+module RecipientsHelper
+	def relationship
+	[
+		['Father', 'father'],
+		['Mother', 'mother'],
+		['Sister', 'sister'],
+		['Friend', 'friend'],
+		['Brother', 'brother'],
+		['Daughter', 'daughter'],
+		['Son', 'son'],
+		['Husband', 'husband'],
+		['Wife', 'wife'],
+		['Boyfriend', 'boyfriend'],
+		['Girlfriend', 'girlfriend'],
+		['Manager', 'manager'],
+		['Grandmother', 'grandmother'],
+		['Grandfather', 'grandfather'],
+		['Grandson', 'grandson'],
+		['Granddaughter', 'granddaughter'],
+		['Niece', 'niece'],
+		['Nephew', 'nephew'],
+		['Aunt', 'aunt'],
+		['Uncle', 'uncle'],
+		['Cousin', 'cousin'],
+		['Colleague', 'colleague']
+	]
+	end
+end
